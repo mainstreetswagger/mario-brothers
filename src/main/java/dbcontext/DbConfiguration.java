f1dbeea7911a05e5e516a5b5fc3b039ff0a54b8c
Kanat <mainstreetswagger@gmail.com>
2024-02-06 18:07:22 +0100

    removed pass

--- src/main/java/dbcontext/DbConfiguration.java
+++ src/main/java/dbcontext/DbConfiguration.java
@@ -2,6 +2,6 @@ package dbcontext;
 
 public class DbConfiguration {
     public static String user = "root";
-    public static String password = "0312697052aA.";
+    public static String password = "";
     public static String url = "jdbc:mysql://localhost:3306/mariobrothers";
 }
